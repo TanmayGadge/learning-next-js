20c5d7507647ab3b3d662255ff095d57c27b20e1
Tanmay Gadge <tanmaygadge9273@gmail.com>
2024-09-22 22:15:22 +0530

    next dashboard app

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-dvh">
+        <p className="" style={{fontSize: "32px"}}>Dashboard Page</p>
+      </div>
+    </>
+  );
+}
